ca4c8c60dfeb40e4c9d5eecec0b375d16063caf1
DeniD-star <dimitrova.denise@gmail.com>
2022-10-31 13:56:50 +0100

    validation errors

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,28 @@
 const router = require('express').Router();
+const {check, validationResult} = require('express-validator');
 const { isGuest, isAuth } = require('../middlewares/guards');
 
 router.get('/register', isGuest(), (req, res) => {
     res.render('register', { title: 'Register Page' })
 })
-router.post('/register', isGuest(), async (req, res) => {//tova avtomati4no stava async function, tui kato, za6toto realno userServica vru6ta asynxronna finkziq i zatova imame try/catch, poneje po vreme na izvikvaneto i moje da izleze nqkakva gre6ka na survura, ina4e node.js 6te ni se kara ako nqmame try/catch
+router.post('/register',
+isGuest(), 
+check('email', 'Please enter a valid email!').isEmail(), 
+check('username', 'Username must be at least 3 characters long!').isLength({min: 3}),
+async (req, res) => {//tova avtomati4no stava async function, tui kato, za6toto realno userServica vru6ta asynxronna finkziq i zatova imame try/catch, poneje po vreme na izvikvaneto i moje da izleze nqkakva gre6ka na survura, ina4e node.js 6te ni se kara ako nqmame try/catch
     try {
-        await req.auth.register(req.body);
+            const {errors} = validationResult(req);
+            if(errors.length > 0){
+                throw new Error(errors.map(e=> e.msg).join('\n'))//da ni pokazva vsi4ki gre6ki navednuj
+            }
+     
+        // await req.auth.register(req.body);
         res.redirect('/products')
     } catch (err) {
         const ctx = {
             title: 'Register',
-            error: err.message,
-            data: { username: req.body.username }
+            errors: err.message.split('\n'),
+            data: { username: req.body.username , email: req.body.email}
         }
         res.render('register', ctx)
     }
